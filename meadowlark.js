b27cf888c6961c1c2f631a5fea2897c0c23c0575
Deepak <deepakworkpc@gmail.com>
2024-08-07 23:35:07 +0530

    feat: add handlebars

--- meadowlark.js
+++ meadowlark.js
@@ -1,7 +1,26 @@
 let express = require("express");
+let handlebars = require("express3-handlebars").create({
+    defaultLayout: "main"
+});
+
 let app = express();
 
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.static(__dirname + "/public"));
+
 app.set("port", process.env.PORT || 5000);
+
+app.get("/", (req, res) => {
+    res.type("text/plain");
+    res.send("MeadowLark Travel");
+})
+
+app.get("/about", (req, res) => {
+    res.type("text/plain");
+    res.send("About MeadowLark Travel")
+})
 
 app.use((req, res) => {
     res.type("text/plain");
